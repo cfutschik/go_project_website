a92514c40ca70123e0331ba960e137fb64624f8c
cfutschik <christian@ronja.tech>
2025-02-16 17:11:49 +0100

    add db + migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/cfutschik/go_project_website.git/internal/db"
 	"github.com/cfutschik/go_project_website.git/internal/env"
 	"github.com/cfutschik/go_project_website.git/internal/env/store"
 )
@@ -10,9 +11,26 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":4001"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://christianfutschik:adminpassword@localhost/myprojectdb?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panicln("new db: %w", err)
+	}
+	log.Printf("db connection established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
---
+++ internal/db/db.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return nil, fmt.Errorf("sql connection: %w", err)
+	}
+	defer db.Close()
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		return nil, fmt.Errorf("time parsing: %w", err)
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("could not ping db: %w", err)
+	}
+
+	return db, nil
+}
