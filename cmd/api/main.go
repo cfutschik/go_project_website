88ff1888316af0fef996a27bbcff1b458dca531e
cfutschik <christian@ronja.tech>
2025-03-20 20:42:07 +0100

    added payload validation via validator

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,7 +14,7 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":4001"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres://christianfutschik:adminpassword@localhost/myprojectdb?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://christianfutschik:adminpassword@project-postgres-db:5434/myprojectdb?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
